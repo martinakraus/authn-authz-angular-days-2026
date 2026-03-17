81bb128d773a176b92cf3c5995d0785dc55edf1a
Martina Kraus <kraus.martina.m@gmail.com>
2026-03-17 08:55:39 +0100

    Clear Keycloak configuration values
    Updated Keycloak configuration to use empty strings.

--- public-client/src/app/auth/auth.config.ts
+++ public-client/src/app/auth/auth.config.ts
@@ -1,7 +1,7 @@
 import { KeycloakConfig } from 'keycloak-js';
 
 export const keycloakConfig: KeycloakConfig = {
-	url: 'http://localhost:8080', // Keycloak URL
-	realm: 'angular-days',              // Your realm name
-	clientId: 'demo' // Your client ID
+	url: '', // Keycloak URL
+	realm: '',              // Your realm name
+	clientId: '' // Your client ID
 };
